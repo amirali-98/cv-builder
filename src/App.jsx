94e4b34e3db1da1120f6ae20a4edc657f3057f93
amirali-98 <amiralimansouri98@outlook.com>
2025-11-17 17:28:35 +0330

    Add routing and BuilderPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import BuilderPage from "./pages/BuilderPage";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/builder" element={<BuilderPage />} />
+        <Route path="*" element={<Navigate to="/builder" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
